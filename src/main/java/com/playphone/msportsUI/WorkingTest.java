ee5856396ffc1075d33034073a83b35c07c46418
siddharthvartak <siddharth.vartak@playphone.com>
2017-02-21 13:49:56 -0800

    firefox binary

--- src/main/java/com/playphone/msportsUI/WorkingTest.java
+++ src/main/java/com/playphone/msportsUI/WorkingTest.java
@@ -1,14 +1,22 @@
 package com.playphone.msportsUI;
 
+import java.io.File;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.testng.annotations.Test;
 
 public class WorkingTest {
 	
 	@Test
 	public void ti () throws InterruptedException {
-		WebDriver driver;
+		File pathToBinary = new File("/Applications/Firefox.app/Contents/MacOS/firefox");
+		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+		FirefoxProfile firefoxProfile = new FirefoxProfile();       
+		WebDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
+		//WebDriver driver;
 		//System.setProperty("webdriver.gecko.driver", "/Users/siddharth/bin/geckodriver");
 		//DesiredCapabilities capabilities=DesiredCapabilities.firefox();
 		//capabilities.setCapability("marionette", true);
