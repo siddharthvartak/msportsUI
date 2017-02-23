63b3ae90aa04954cbb436914c79439a91dc42964
siddharthvartak <siddharth.vartak@playphone.com>
2017-02-23 10:33:19 -0800

    Adding new test cases

---
+++ src/main/java/com/playphone/msportsUI/AccountLoginNegative.java
@@ -0,0 +1,29 @@
+package com.playphone.msportsUI;
+
+import java.net.URL;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+
+public class AccountLoginNegative {
+
+
+@Test
+
+public void ti () throws MalformedURLException {
+String hubURL = "http://172.16.1.118:6577/wd/hub";
+DesiredCapabilities capability = DesiredCapabilities.firefox();
+WebDriver driver = new RemoteWebDriver(new URL(hubURL), capability);
+driver.get("http://games.o01.dev.playphone.cc/#/");
+
+
+
+driver.quit();
+
+
+}
+
+
+}
