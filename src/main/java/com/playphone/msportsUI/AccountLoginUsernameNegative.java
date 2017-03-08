2b61b51ce30175f0034da70fa67101e14fc5221d
siddharthvartak <siddharth.vartak@playphone.com>
2017-03-08 15:18:57 -0800

    changes to accountusernamenegative test case

--- src/main/java/com/playphone/msportsUI/AccountLoginUsernameNegative.java
+++ src/main/java/com/playphone/msportsUI/AccountLoginUsernameNegative.java
@@ -26,13 +26,8 @@ public class AccountLoginUsernameNegative {
 			DesiredCapabilities capability = DesiredCapabilities.firefox();
 			driver = new RemoteWebDriver(new URL("http://172.16.1.68:4444/wd/hub"), capability);
 			driver.get("https://tournaments.o01.dev.playphone.cc/#/");
-<<<<<<< HEAD
-			
-	//WebDriver driver;  
-=======
 
 	//WebDriver driver;
->>>>>>> master
 	//System.setProperty("webdriver.gecko.driver", "/Users/siddharth/Documents/workspace/geckodriver");
     //driver = new FirefoxDriver();
     //baseUrl = "http://tournaments.olympus01.playphone.cc/";
@@ -43,14 +38,6 @@ public class AccountLoginUsernameNegative {
   public void TestInvalidUsername () throws MalformedURLException {
 
 	  // driver.get(baseUrl + "/#/");
-<<<<<<< HEAD
-	  
-	    new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//button[@type='button'])[3]")));
-	    driver.findElement(By.xpath("(//button[@type='button'])[3]")).click();
-	    new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@type='button']")));
-		driver.findElement(By.xpath("//button[@type='button']")).click();
-		new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[@data-pp-auto-id='login-with-google'][contains(text(),'Log in with Email')]")));
-=======
 	  try {
           Thread.sleep(3000);
       } catch (InterruptedException e) {
@@ -79,7 +66,6 @@ public class AccountLoginUsernameNegative {
             e.printStackTrace();
         }
 		new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[@data-pp-auto-id='login-with-google'][contains(text(),'Log in with Email')]")));
->>>>>>> master
 	    driver.findElement(By.xpath("//span[@data-pp-auto-id='login-with-google'][contains(text(),'Log in with Email')]")).click();
 	    //(".//*[@id='play-now-button-1']")).click();
 	    new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.name("email")));
